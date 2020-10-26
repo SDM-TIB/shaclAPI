c35448e76a832a9c9a1d29e2f70c6f9c9e581650
Julian Gercke <julian.gercke@gmx.de>
2020-10-26 09:28:50 +0100

    Query Debuging

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, Response
-from SPARQLWrapper import SPARQLWrapper
+from SPARQLWrapper import SPARQLWrapper, XML
 from rdflib import ConjunctiveGraph
 from rdflib.plugins.serializers.nquads import NQuadsSerializer
+from rdflib.plugins import sparql
+from rdflib.plugins.sparql.results.xmlresults import XMLResultParser
 import json
 
 app = Flask(__name__)
@@ -15,10 +17,18 @@ def querySubgraph():
     if request.method == 'GET':
         printArgs(request.args)
         query = request.args['query']
+    
+    external_result = query_external_endpoint(query)
     query = query.replace(","," ")
+    parsed_query = sparql.processor.prepareQuery(query)
+    print(parsed_query.__dict__)
+
     subgraph = getSubgraph()
     if subgraph == None:
-        return "Set Construct Query first..."    
+        return "Set Construct Query first..." 
+
+    subgraph.parse(external_result, format="xml")
+
     result = subgraph.query(query)
 
     return Response(result.serialize(encoding='utf-8',format='json'), mimetype='application/json')
@@ -32,7 +42,7 @@ def retrieveSubgraph():
 def setSubgraph(subgraph):
     with open("graph.nquads", "wb") as f:
         NQuadsSerializer(subgraph).serialize(f)
-        
+     
 def getSubgraph():
     g = ConjunctiveGraph()
     with open("graph.nquads", "rb") as f:
@@ -44,3 +54,16 @@ def printArgs(args):
         print('------------------' + str(k) + '------------------\n')
         print(v)
         print('\n')
+
+def custom_evaluation_of_sparql(ctx,part):
+    print("Hello World")
+    print(str(ctx) + '\n' + str(part))
+    raise NotImplementedError
+
+def query_external_endpoint(query):
+    endpoint.setQuery(query)
+    result = endpoint.query().convert()
+    print(result)
+    return XMLResultParser.parse(result.toxml(encoding='utf-8'))
+
+#sparql.CUSTOM_EVALS["test"] = custom_evaluation_of_sparql
