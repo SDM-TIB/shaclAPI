c385021cafc7573317656057dd16a8ed991431dc
fabian <fabian.johannsen@stud.uni-hannover.de>
2020-10-29 11:32:53 +0100

    Adapted test.sh

--- test.sh
+++ test.sh
@@ -7,3 +7,4 @@ sleep 2s
 curl -X POST -H "Accept:application/sparql-results+json" --data-urlencode "query=$QUERY" http://127.0.0.1:5000/endpoint 2>&1 >/dev/null
 sleep 2s
 python3 travshacl/main.py -d ./shapes/3 -a http://localhost:5000/endpoint ./output/ BFS --heuristics TARGET IN BIG --orderby --selective
+python3 query_parser.py -d ./shapes/3 "query=$QUERY"
