8f6fe2ee3547ae7d82d8aff74358541968a5d72a
fabian <fabian.johannsen@stud.uni-hannover.de>
2020-12-08 18:50:43 +0100

    Optional param (dir, default:pwd)

--- tests/setup/create_endpoint_test.sh
+++ tests/setup/create_endpoint_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+if [ $# -gt 1 ]; then
+    path="$(pwd)/TestData"
+else
+    path=$1
+fi
 sudo docker stop test_graph
 sudo docker rm test_graph
-sudo docker run --name test_graph -p 14000:8890 -v "$(pwd)/TestData:/data/toLoad" -d kemele/virtuoso:7-stable
+sudo docker run --name test_graph -p 14000:8890 -v "$path:/data/toLoad" -d kemele/virtuoso:7-stable
