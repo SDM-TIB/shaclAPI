daa7ca0ea1d0e8ff19c7ecb87214d2a205b4b5ed
Julian Gercke <julian.gercke@gmx.de>
2021-02-18 14:52:02 +0100

    Added git Patch File and script to be applied after update of travshacl submodule

---
+++ apply_patch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git config -f .gitmodules submodule.SDM-SHACL.branch Trav-SHACL
+git submodule update --remote
+cd travshacl
+git checkout Trav-SHACL
+git branch -D local
+git branch local
+git checkout local
+git am --abort
+git am ../0001-required-changes.patch
+cd ..
