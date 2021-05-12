0e8b0246c4dec1428fefcc3e066fe80cdb6ea122
Philipp Rohde <Philipp.Rohde@tib.eu>
2021-05-12 09:24:16 +0200

    rename columns of different outputs

--- app/output/statsCalculation.py
+++ app/output/statsCalculation.py
@@ -1,10 +1,12 @@
 import time
-from app.output.statsOutput import StatsOutput
-from app.output.CSVWriter import CSVWriter
 import warnings
-import math
 
-class StatsCalculation():
+from app.output.CSVWriter import CSVWriter
+from app.output.statsOutput import StatsOutput
+
+
+class StatsCalculation:
+
     def __init__(self, test_identifier, approach_name):
         self.test_name = test_identifier
         self.approach_name = approach_name
@@ -30,9 +32,12 @@ class StatsCalculation():
         result_stat = individual_result_times_queue.get(timeout=queue_timeout)
         received_results = 0
         while result_stat != 'EOF':
-            if result_stat['topic'] ==  'new_xjoin_result':
+            if result_stat['topic'] == 'new_xjoin_result':
                 received_results += 1
-                writer.writeMulti({"test_name": self.test_name, "approach": self.approach_name, "time": result_stat['time'] - self.global_start_time, "validation": 'valid' if result_stat['validation_result'] else 'invalid'})
+                writer.writeMulti({"test": self.test_name,
+                                   "approach": self.approach_name,
+                                   "answer": received_results,
+                                   "time": result_stat['time'] - self.global_start_time})
                 self.last_result_timestamp = result_stat['time']
                 if not self.first_result_timestamp:
                     self.first_result_timestamp = result_stat['time']
@@ -45,9 +50,11 @@ class StatsCalculation():
             warnings.warn("Number of Result timestamps received is not equal to the number of results!")
         writer.close()
 
-    
     def receive_global_stats(self, stats_out_queue, queue_timeout):
-        needed_stats = {'mp_validate': False, 'contactSource': False, 'mp_xjoin': False, 'first_validation_result': False }
+        needed_stats = {'mp_validate': False,
+                        'contactSource': False,
+                        'mp_xjoin': False,
+                        'first_validation_result': False}
         while sum(needed_stats.values()) < len(needed_stats.keys()):
             statistic = stats_out_queue.get(timeout=queue_timeout)
             needed_stats[statistic['topic']] = True
@@ -60,10 +67,9 @@ class StatsCalculation():
             elif statistic['topic'] == 'first_validation_result':
                 self.join_started_time = statistic['time']
             elif statistic['topic'] == 'Exception':
-                raise Exception("An Exception occured in " + statistic['location'])
+                raise Exception("An Exception occurred in " + statistic['location'])
             else:
                 raise Exception("received statistic with unknown topic")
-
 
     def write_matrix_and_stats_files(self, matrix_file, stats_file):
         total_execution_time = self.global_end_time - self.global_start_time
@@ -81,8 +87,17 @@ class StatsCalculation():
         else:
             last_result_time = "NaN"
 
-        matrix_entry = {"test_name": self.test_name, "approach": self.approach_name, "first_result_time": first_result_time, "last_result_time": last_result_time, "number_of_results": self.number_of_results}
-        stats_entry = {"test_name": self.test_name, "approach": self.approach_name, "total_execution_time": total_execution_time, "query_time": query_time, "network_validation_time": network_validation_time, "join_time": join_time}
+        matrix_entry = {"test": self.test_name,
+                        "approach": self.approach_name,
+                        "tfft": first_result_time,
+                        "totaltime": last_result_time,
+                        "comp": self.number_of_results}
+        stats_entry = {"test": self.test_name,
+                       "approach": self.approach_name,
+                       "total_execution_time": total_execution_time,
+                       "query_time": query_time,
+                       "network_validation_time": network_validation_time,
+                       "join_time": join_time}
         CSVWriter(matrix_file).writeSingle(matrix_entry)
         CSVWriter(stats_file).writeSingle(stats_entry)
         return StatsOutput(output=stats_entry)
