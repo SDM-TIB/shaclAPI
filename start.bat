65e02f5876706f24c20eaf8c7bcca19ea1b0fb0f
hany2 <hany.alom@gmail.com>
2022-06-27 15:16:06 +0200

    new example with compressed data

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 set FLASK_APP=run.py
 set FLASK_ENV=production
-py -m flask run --host 0.0.0.0 --port 5000
+py -m flask run --host 0.0.0.0 --port 9999
---
+++ startdocker.bat
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose.yml up -d
+docker-compose -f ./tests/docker-compose.yml up -d
