88f324989f730cce176f042a6b77f2637baa185c
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-07-13 10:30:01 +0200

    fix S2Spy prefix handling

--- shaclapi/reduction/__init__.py
+++ shaclapi/reduction/__init__.py
@@ -2,22 +2,18 @@ from shaclapi.reduction.travshacl.ReducedShapeSchema import ReducedShapeSchema a
 from shaclapi.reduction.s2spy.ReducedShapeSchema import ReducedShapeSchema as ReducedShapeSchemaS2Spy
 from TravSHACL.sparql.SPARQLEndpoint import SPARQLEndpoint
 
-import validation.sparql.SPARQLPrefixHandler as SPARQLPrefixHandler
 
 def prepare_validation(config, query, result_transmitter):
-    '''
+    """
     Given a Config Object (app/config.py), a Query Object (app/query.py) and an Result Transmitter Object, 
     which will be used to for non-blocking transmission of validation results from the backend to the api,
     this methode will prepare a matching ShapeSchema to be used for validation.
-    '''
-
+    """
     # Prepare the backend and choose the matching inherited ShapeSchema.
     if config.backend == "travshacl":
         ShapeSchema = ReducedShapeSchemaTravShacl
         SPARQLEndpoint.instance = None # The SPARQLEndpoint Object of Travshacl is a singleton, so we need to rest it otherwise it won't change
     elif config.backend == "s2spy":
-        SPARQLPrefixHandler.prefixes = {str(key): "<" + str(value) + ">" for (key, value) in query.namespace_manager.namespaces()}
-        SPARQLPrefixHandler.prefixString = "\n".join(["".join("PREFIX " + key + ":" + value) for (key, value) in SPARQLPrefixHandler.prefixes.items()]) + "\n"
         ShapeSchema = ReducedShapeSchemaS2Spy
     else:
         raise NotImplementedError("The given backend {} is not implemented".format(config.backend))
