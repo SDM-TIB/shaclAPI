88222eecba20c4c675012dd5fb5450e4a80aa769
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-08-01 11:14:16 +0200

    update setup.py
    updating package name, package content, and dependencies

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='travshaclAPI',
-      version='1.0',
-      description='travshaclAPI',
-      packages=find_packages(),
-      install_requires=['SPARQLWrapper','flask','rdflib','requests','pyinstrument','PyYaml'],
-      include_package_data=True,
+setup(name='shaclapi',
+      version='1.0.0',
+      description='shaclapi',
+      packages=find_packages(exclude='tests'),
+      install_requires=[
+            'SPARQLWrapper==2.0.0',
+            'requests==2.27.1',
+            'SHACL2SPARQLpy==1.1.0',
+            'TravSHACL==1.0.0',
+            'multiprocessing_logging==0.3.1',
+            'regex==2022.3.15'
+      ],
+      include_package_data=False,
       python_requires='>=3.8',
       zip_safe=False)
