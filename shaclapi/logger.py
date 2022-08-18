30105e49e0a032752e15db9dac00938e368fa459
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-08-18 09:30:40 +0200

    style improvements in logger.py

--- shaclapi/logger.py
+++ shaclapi/logger.py
@@ -3,19 +3,21 @@ import logging
 from multiprocessing_logging import install_mp_handler
 
 
-def setup(handler=logging.StreamHandler(), level=logging.INFO, format='[%(asctime)s - %(levelname)s] %(name)s - %(processName)s: %(msg)s'):
-    """Sets up the logger of the shaclAPI. Has to be used before importing shaclapi.api.
+def setup(handler=logging.StreamHandler(),
+          level=logging.INFO,
+          log_format='[%(asctime)s - %(levelname)s] %(name)s - %(processName)s: %(msg)s'):
+    """Sets up the shaclAPI's logger. Has to be used before importing shaclapi.api.
 
     Parameters
     ----------
-        handler : logging.Handler, optional
-            The handler to be used for logging of the shaclapi
-        level : logging._LEVEL, optional
-            The logging level
-        format : str, optional
-            The format to be used during logging.
+    handler : logging.Handler, optional
+        The handler to be used for logging of the shaclapi
+    level : logging._LEVEL, optional
+        The logging level
+    log_format : str, optional
+        The format to be used during logging.
     """
-    handler.setFormatter(logging.Formatter(format))
+    handler.setFormatter(logging.Formatter(log_format))
     handler.setLevel(level)
     logger = logging.getLogger('shaclapi')
     logger.addHandler(handler)
