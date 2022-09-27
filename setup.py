0568c7f5929f0f65fc9219de7bb60bccae9a2e25
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-09-27 15:57:45 +0200

    release of v0.9.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-VERSION = '0.9.3'
+VERSION = '0.9.4'
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
