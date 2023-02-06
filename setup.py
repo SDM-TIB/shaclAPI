637e5fd5137e53b8b7935235c956beb5343e8c39
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-02-06 16:42:23 +0100

    require a newer version of Trav-SHACL

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'SPARQLWrapper>=2.0.0',
         'requests>=2.27.1',
         'SHACL2SPARQLpy>=1.1.1',
-        'TravSHACL>=1.0.2',
+        'TravSHACL>=1.2.0',
         'multiprocessing_logging>=0.3.1',
         'regex>=2022.3.15'
     ],
