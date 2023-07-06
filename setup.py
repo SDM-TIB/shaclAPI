c5153792d6967ef049ef766882e5d3e16931aa89
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-07-06 10:20:40 +0200

    require newer version of SHACL2SPARQLpy

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         'SPARQLWrapper>=2.0.0',
         'requests>=2.31.0',
-        'SHACL2SPARQLpy>=1.1.1',
+        'SHACL2SPARQLpy>=1.2.0',
         'TravSHACL>=1.3.0',
         'multiprocessing_logging>=0.3.1',
         'regex>=2022.3.15'
