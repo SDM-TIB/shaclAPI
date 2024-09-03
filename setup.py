8b44c813344ba9789f5cd7d7768ad68206447a65
Philipp Rohde <Philipp.Rohde@tib.eu>
2024-09-03 08:25:38 +0200

    [dep] set minimal version of requests to v2.32.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     packages=find_packages(exclude='tests'),
     install_requires=[
         'SPARQLWrapper>=2.0.0',
-        'requests>=2.31.0',
+        'requests>=2.32.0',
         'SHACL2SPARQLpy>=1.3.0',
         'TravSHACL>=1.7.0',
         'multiprocessing_logging>=0.3.1',
